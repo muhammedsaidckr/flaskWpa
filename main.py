4aa0d6fd10698e19b690d8e17e4258073160a8cf
muhammedsaidckr <muhammedsaidckr@gmail.com>
2024-03-04 11:18:09 +0300

    Rendered html page with basic css

--- main.py
+++ main.py
@@ -1,65 +1,39 @@
-
-
-from flask import Flask,request
+from flask import Flask, request, render_template
 import subprocess
 
 app = Flask(__name__)
 
-wifi_device = "wlan0"
+wifi_device = "wlp0s20f3"
+
 
 @app.route('/')
 def index():
-    result = subprocess.check_output(["nmcli", "--colors", "no", "-m", "multiline", "--get-value", "SSID", "dev", "wifi", "list", "ifname", wifi_device])
+    result = subprocess.check_output(
+        ["nmcli", "--colors", "no", "-m", "multiline", "--get-value", "SSID", "dev", "wifi", "list", "ifname",
+         wifi_device])
     ssids_list = result.decode().split('\n')
-    dropdowndisplay = f"""
-        <!DOCTYPE html>
-        <html>
-        <head>
-            <title>Wifi Control</title>
-        </head>
-        <body>
-            <h1>Wifi Control</h1>
-            <form action="/submit" method="post">
-                <label for="ssid">Choose a WiFi network:</label>
-                <select name="ssid" id="ssid">
-        """
-    for ssid in ssids_list:
-        only_ssid = ssid.removeprefix("SSID:")
-        if len(only_ssid) > 0:
-            dropdowndisplay += f"""
-                    <option value="{only_ssid}">{only_ssid}</option>
-            """
-    dropdowndisplay += f"""
-                </select>
-                <p/>
-                <label for="password">Password: <input type="password" name="password"/></label>
-                <p/>
-                <input type="submit" value="Connect">
-            </form>
-        </body>
-        </html>
-        """
-    return dropdowndisplay
+    ssids_cleaned = [ssid.removeprefix("SSID:") for ssid in ssids_list if len(ssid.removeprefix("SSID:")) > 0]
+    return render_template('index.html', ssids=ssids_cleaned)
 
 
-@app.route('/submit',methods=['POST'])
+@app.route('/submit', methods=['POST'])
 def submit():
     if request.method == 'POST':
-        print(*list(request.form.keys()), sep = ", ")
+        print(*list(request.form.keys()), sep=", ")
         ssid = request.form['ssid']
         password = request.form['password']
         connection_command = ["nmcli", "--colors", "no", "device", "wifi", "connect", ssid, "ifname", wifi_device]
         if len(password) > 0:
-          connection_command.append("password")
-          connection_command.append(password)
+            connection_command.append("password")
+            connection_command.append(password)
         result = subprocess.run(connection_command, capture_output=True)
-        if result.stderr:
-            return "Error: failed to connect to wifi network: <i>%s</i>" % result.stderr.decode()
-        elif result.stdout:
-            return "Success: <i>%s</i>" % result.stdout.decode()
-        return "Error: failed to connect."
+        message = ("Success: Connected to <i>{}</i>".format(ssid)) \
+            if result.returncode == 0 \
+            else "Error: Failed to connect to <i>{}</i>".format(ssid)
+        # return if it is error or success
+        return render_template('result.html', message=message, returncode=result.returncode)
+    return "Error: HTTP Method not allowed."
 
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=80)
-
